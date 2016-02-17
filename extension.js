4c74bec787458ca4baafd762dc352be46d358de4
Alex <thisisbanana@hotmail.com>
2016-02-17 22:49:11 +1000

    Refactoring

--- extension.js
+++ extension.js
@@ -1,45 +1,75 @@
+
 /**
  * Created by yolomachine on 10.02.2016.
- * WORK IN PROGRESS
  */
 
-var postScan = detect_posts();
-var news = l_nwsf;
-
-function Window(lastURL, oldURL){
-    this.currentURL  = lastURL;
-    this.previousURL = oldURL;
-}
-
-Window.prototype.refresh = function(href){     // href == window.location.href --
-    this.targetURL = "vk.com/feed";            // setInterval(Window.refresh(window.location.href), 1000);
-    switch(href){
-        case this.currentURL:
-            if (href.search( this.targetURL ) !== -1 && href === this.previousURL)
-                break
-            else
-            if (href.search( this.targetURL ) !== -1 && href !== this.previousURL)
-                postScan();
-            break;
-        case this.previousURL:
-            this.previousURL = this.currentURL;
-            this.currentURL  = href;
-            break;
-    }
-}
-
 appAPI.ready(function($){
-    var activeTab = new Window(window.location.href, "");
-    postScan;
-    setInterval(activeTab.refresh(window.location.href), 1000);
+    var currentURL = window.location.href;
+    var postCount = 0;
 
-    news.onclick= function(){
-        postScan;
+    function Window(){
+        this.targetURL = "vk.com/feed";
+    }
+
+    Window.prototype.check = function(href){
+        if (href.search( this.targetURL ) !== -1)
+            return true;
+    }
+
+    Window.prototype.refresh = function(){     // href == window.location.href -- setInterval(Window.refresh(window.location.href), 1000)
+        if (this.check(currentURL))
+            if (postCount != $(".post").length) {
+                detectPosts();
+                postCount = $(".post").length;
+            }
+    }
+
+    var activeTab = new Window();
+    detectPosts();
+
+    l_nwsf.onclick = function(){
+        activeTab.refresh()
     }
 
     window.onscroll = function(){
-        if (postCount != $(".post").length){
-            postScan;
-            postCount = $(".post").length;
+        activeTab.refresh()
+    }
+
+    function detectPosts(){
+        if (activeTab.check(currentURL)){
+            $(".post").each(function(){
+
+                var id = $(this).attr("id");
+
+                var postText = ($(this).find(".wall_post_text").text()) + ' ' + ($(this).find(".wall_post_text").find("span").text());  // text of user and text of repost
+                var authorID = ($(this)).find(".wall_text_name").find("a").attr('href');
+                var authorName = ($(this)).find(".wall_text_name").find("a").text();
+                var relDate = ($(this)).find(".rel_date").text();
+
+                var repAuthorID = ($(this)).find(".published_by").attr("href");
+                var repAuthorName  = ($(this)).find(".published_by").text();
+
+                var commercial = $(this).find(".wall_text_name_explain_promoted_post").text();
+
+
+                console.log("r_author " + repAuthorName +  " " + repAuthorID);
+                console.log("text_p " + postText);
+                console.log("author_p " + authorName + " " + authorID);
+                console.log("rel_date " + relDate);
+                console.log("commercial " + commercial);
+
+                appAPI.request.get({
+                    url: 'http://5.100.72.142:3000/spamdetect?text=' + postText,   // адрес сервера http://5.100.72.142:3000/
+                    onSuccess: function(response, additionalInfo) {
+                        var tmp = JSON.parse(response);
+                        if (tmp["verdict"] === "SPAM"){
+                            $("#" + id).hide();
+                            id_spam[id_spam.length] = id;
+                            console.log(id_spam.length);
+                        }
+                    }
+                });
+            });
         }
     }
+});
