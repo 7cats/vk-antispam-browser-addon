c9db89168ad0328a1e6c22a077197da32e430b08
yolomachine <thisisbanana@hotmail.com>
2016-02-20 17:45:59 +1000

    Added files via upload

---
+++ background.js
@@ -0,0 +1,9 @@
+/************************************************************************************
+  This is your background code.
+  For more information please visit our wiki site:
+  http://docs.crossrider.com/#!/guide/scopes_background
+*************************************************************************************/
+
+appAPI.ready(function($) {
+
+});
--- extension.js
+++ extension.js
@@ -1,75 +1,82 @@
-appAPI.ready(function($) {
-  var old_change_URL = window.location.href;
-  var new_change_URL = "";
-  var nowURL = window.location.href;
-  var which_url = 1;
-  var count_posts = 0;
-  var id_spam = [];
-  detect_posts();
-  //каждую 1 секунду чекаем, не изменился  ли url, если перешли на /feed, то чекаем посты.
-  setInterval(function() {
-    if (which_url == 1){
-      new_change_URL = window.location.href;
-      nowURL = new_change_URL;
+
+/**
+ * Created by yolomachine on 10.02.2016.
+ */
+
+appAPI.ready(function($){
+    var currentURL = window.location.href;
+    var postCount  = 0;
+    var xhr = new XMLHttpRequest();
+
+    function Window(){
+        this.targetURL = "vk.com/feed";
     }
-    else{
-      old_change_URL = window.location.href;
-      nowURL = old_change_URL;
+
+    Window.prototype.check = function(href){
+        if (href.search( this.targetURL ) !== -1)
+            return true;
     }
-    if (new_change_URL !== old_change_URL && (nowURL === 'http://vk.com/feed' || nowURL === 'http://vk.com/al_feed' )){
-      detect_posts();
+
+    Window.prototype.refresh = function(){
+        if (this.check(currentURL)){
+            postCount  = $(".post").length;
+            detectPosts();
+        }
     }
-    which_url ^= 1;
-  }, 1000);
-  // если нажали на news (на случай перехода feed -> feed)
-  l_nwsf.onclick = function (){
-    detect_posts();
-  }
-  //если скролл, то смотрим все записи, если новые появились
-  window.onscroll = function() {
-    if (count_posts != $(".post").length){
-      detect_posts();
-      count_posts = $(".post").length;
+
+    var activeTab = new Window();
+
+    l_nwsf.onclick = function(){
+        activeTab.refresh()
     }
-  }
 
+    var target = document.querySelector("#show_more_progress");
 
-  function detect_posts(){
-    if (nowURL === 'http://vk.com/feed' || nowURL === 'http://vk.com/al_feed'){
-      $(".post").each(function(){
-
-        var id = $(this).attr("id");
-
-        var text_post = ($(this).find(".wall_post_text").text()) + ' ' + ($(this).find(".wall_post_text").find("span").text());  // text of user and text of repost
-        var au_id = ($(this)).find(".wall_text_name").find("a").attr('href');
-        var au_name = ($(this)).find(".wall_text_name").find("a").text();
-        var rel_date = ($(this)).find(".rel_date").text();
-
-        var r_au_id = ($(this)).find(".published_by").attr("href");
-        var r_au_name = ($(this)).find(".published_by").text();
-
-        var commercial = $(this).find(".wall_text_name_explain_promoted_post").text();
-
-
-        /*console.log("r_author " + r_au_name +  " " + r_au_id);
-         console.log("text_p " + text_post);
-         console.log("author_p " + au_name + " " + au_id);
-         console.log("rel_date " + rel_date);
-         console.log("commercial " + commercial);*/
-
-        appAPI.request.get({
-          url: 'http://5.100.72.142:3000/spamdetect?text=' + text_post,   // адрес сервера http://5.100.72.142:3000/
-          onSuccess: function(response, additionalInfo) {
-            var tmp = JSON.parse(response);
-            if (tmp["verdict"] === "SPAM"){
-              $("#" + id).hide();
-              id_spam[id_spam.length] = id;
-              console.log(id_spam.length);
-            }
-          }
+    var observer = new MutationObserver(function(mutations){
+        mutations.forEach(function(mutation) {
+            console.log(mutation.type);
+            detectPosts();
         });
-      });
-    }
-  }
+    });
 
+    var config = { attributes: true, childList: true, characterData: true };
+    
+    activeTab.refresh()
+    observer.observe(target, config)
+    
+    function detectPosts(){
+        if (activeTab.check(currentURL)){
+            $(".post").each(function(){
+
+                var ID = $(this).attr("id");
+                var spamID = [];
+
+                var commercial     =  $(this).find(".wall_text_name_explain_promoted_post").text();
+                var postText       =  $(this).find(".wall_post_text").text() + " " + $(this).find(".wall_post_text").find("span").text();
+                var relDate        =  $(this).find(".rel_date").text();
+                var authorID       =  $(this).find(".wall_text_name").find("a").attr('href');
+                var authorName     =  $(this).find(".wall_text_name").find("a").text();
+                var repAuthorID    =  $(this).find(".published_by").attr("href");
+                var repAuthorName  =  $(this).find(".published_by").text();
+
+                console.log("commercial " + commercial);
+                console.log("text_p "     + postText);
+                console.log("rel_date "   + relDate);
+                console.log("author_p "   + authorName    + " " + authorID);
+                console.log("r_author "   + repAuthorName + " " + repAuthorID);
+
+                appAPI.request.get({
+                    url: 'http://5.100.72.142:3000/spamdetect?text=' + postText,
+                    onSuccess: function(response, additionalInfo) {
+                        var temp = JSON.parse(response);
+                        if (temp["verdict"] === "SPAM"){
+                            $("#" + id).hide();
+                            spamID[spamID.length] = ID;
+                            console.log(spamID.length);
+                        }
+                    }
+                });
+            });
+        }
+    }
 });
